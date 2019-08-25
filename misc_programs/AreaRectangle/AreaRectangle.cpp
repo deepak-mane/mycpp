30d4a34b897672228b4aea49d745bb6ecb3a892b
Deepak Mane <deepak-mane@users.noreply.github.com>
2019-08-25 07:35:50 -0700

    Create AreaRectangle.cpp

---
+++ misc_programs/AreaRectangle/AreaRectangle.cpp
@@ -0,0 +1,55 @@
+//============================================================================
+// Name        : AreaRectangle.cpp
+// Author      : Deepak Mane
+// Version     : v1.0
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+class Rectangle {
+private:
+	int length;
+	int breadth;
+
+public:
+	// Overloaded Constructors
+	Rectangle() {length=breadth=1;}
+	Rectangle(int l,int b);
+	// Facilitators
+	int area();
+	int perimeter();
+	// Accessor / Getter Function
+	int getlength() {return length;}
+	// Mutator / Setter Function
+	void setlength(int l) {length = l;}
+	// Destructor
+	~Rectangle();
+};
+
+
+Rectangle::Rectangle(int l, int b) {
+	length = l;
+	breadth = b;
+}
+
+int Rectangle::area() {
+	return length * breadth;
+}
+
+int Rectangle::perimeter() {
+	return 2 * (length + breadth);
+}
+Rectangle::Rectangle(int l, int b) {
+
+}
+Rectangle::Rectangle(int l, int b) {
+
+}
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
