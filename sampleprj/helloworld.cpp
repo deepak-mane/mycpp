bf79429eb9708e3305846595d19647417e8de3df
Deepak Mane <deepak-mane@users.noreply.github.com>
2020-02-04 00:33:15 -0800

    Update helloworld.cpp

--- sampleprj/helloworld.cpp
+++ sampleprj/helloworld.cpp
@@ -1,10 +1,6 @@
-#include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <string>
-
-int input();
-void output(float);
 
 using namespace std;
 
@@ -18,64 +14,6 @@ int main()
         cout << word << " ";
     }
     cout << endl;
-
-	float result;
-	int choice, num;
-    printf("        EDUCATIONAL MATH PROGRAM!!!\n");
-    printf("------------------------------------------\n\n");
-	do {
-		printf("Press 1 to calculate area of circle\n");
-		printf("Press 2 to calculate area of square\n");
-		printf("Press 3 to calculate area of sphere\n");
-		printf("Press 9 to Exit\n");
-		printf("Enter your choice:\n");
-		choice = input();
-
-		switch (choice) {
-		case 1: {
-			printf("Enter radius:\n");
-			num = input();
-			result = 3.14 * num * num;
-			printf("Area of sphere=");
-			output(result);
-			printf("\n\nWhat's Next you want to do ...\n");
-			break;
-		}
-		case 2: {
-			printf("Enter side of square:\n");
-			num = input();
-			result = num * num;
-			printf("Area of square=");
-			output(result);
-			printf("\n\n");
-			break;
-		}
-		case 3: {
-			printf("Enter radius:\n");
-			num = input();
-			result = 4 * (3.14 * num * num);
-			printf("Area of sphere=");
-			output(result);
-			printf("\n\n");
-			break;
-		}
-		case 9: {
-			printf("Bye-Bye:\n");
-			break;
-		}
-		default:
-			printf("wrong Input\n");
-		}
-	} while (choice  != 9);
-	return 0;
-}
-int input() {
-	int number;
-	scanf("%d", &number);
-	return (number);
-}
-
-void output(float number) {
-	printf("%f", number);
+    return 0;
 }
 
